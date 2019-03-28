651e23013fd84609a8044e8ab5ce028f9bb74e2d
Dawid Janczyk <45544015+DawidJanczyk@users.noreply.github.com>
2019-03-28 22:39:32 +0100

    Add files via upload

---
+++ Used.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Poradnik
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Used.xaml
+    /// </summary>
+    public partial class Used : Window
+    {
+        public Used()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to close this window?","Exit", MessageBoxButton.YesNo, MessageBoxImage.Stop) == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Settings(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("In the process of making", "Information");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.webBrowser.CanGoBack)
+            {
+                this.webBrowser.GoBack();
+            }
+        }
+    }
+}
